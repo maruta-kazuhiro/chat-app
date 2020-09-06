315ae24a22758d953f4ad0886422088d01c99b3a
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-09-06 22:11:05 +0900

    aaaaaaa

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -1,9 +1,9 @@
 module SignInSupport
   def sign_in(user)
-    visit root_path
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_on("Log in")
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    find('input[name="commit"]').click
     expect(current_path).to eq root_path
   end
 end
